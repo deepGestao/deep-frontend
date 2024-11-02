3c204fbaa1d17968fd75185bd9cd146ef87a6c7c
yagodev <yagodev974@gmail.com>
2024-11-02 14:24:54 -0300

    Ajustando bt da confirmação de email

--- src/components/pages/ConfirmPage.tsx
+++ src/components/pages/ConfirmPage.tsx
@@ -15,7 +15,7 @@ const ConfirmEmailPage = () => {
       <h1 className="text-2xl font-bold">Confirmação de Email</h1>
       <p className="mt-4">Por favor, verifique seu email para confirmar sua conta.</p>
       <button 
-        className="mt-6 px-4 py-2 bg-blue-500 text-white rounded"
+        className="mt-6 px-4 py-2 bg-black rounded-[12px] mb-3 hover:bg-gray-800 transition-colors duration-300 text-white rounded"
         onClick={handleConfirmed}
       >
         Já confirmei
